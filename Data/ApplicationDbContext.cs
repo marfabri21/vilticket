abc00ca119d09fc535b2d673473736b885f0c7d4
Martin Vilche <mfvilche@gmail.com>
2018-12-29 19:34:48 -0300

    test

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vilticket.Models;
+
+namespace Vilticket.Data
+{
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+
+        }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+
+        }
+
+
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM microsoft/dotnet:2.2-sdk
+ENV ASPNETCORE_URLS="http://*:5123"
+WORKDIR /opt/netcore/src
+COPY . ./
+RUN useradd -m net && chown net:net -R /usr/share/dotnet /opt/netcore
+USER net
+RUN dotnet restore && dotnet publish -c Release -o app && mv app /opt/netcore/ && rm -rf /opt/netcore/src
+EXPOSE 5123
+WORKDIR /opt/netcore/app
+ENTRYPOINT ["dotnet", "Personal.dll"]
