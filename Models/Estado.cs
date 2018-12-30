a07236ffc7bd43ed09b05b209647cf727e82aa4a
Martin Vilche <mfvilche@gmail.com>
2018-12-29 23:15:32 -0300

    test

---
+++ Models/Estado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vilticket.Models
+{
+    [Table("Estado")]
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vilticket.Models
+{
+    public class Ticket
+    {
+
+        public int Id { get; set; }
+        public string Asunto { get; set; }
+        public string Detalle { get; set; }        
+        public DateTime FechaCreacion { get { return DateTime.Now; } set {} }
+        public TimeSpan TiempoTranscurrido { get; set; }
+        public int IdEstado { get; set; }
+        [ForeignKey("IdEstado")]
+        public Estado Estado { get; set; }
+    }
+}
